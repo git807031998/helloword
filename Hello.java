bd2b4db1791814a99da115a16234d5a560e9de59
git807031998 <30403607+git807031998@users.noreply.github.com>
2020-04-27 21:50:14 +0800

    删除了两个打印语句
    删除hello和world的打印

--- Hello.java
+++ Hello.java
@@ -1,7 +1,6 @@
 public class Hello{
   public static void main(String []args){
-    System.out.println("hello...");
-    System.out.println("world...");
+    
     System.out.println("hello world");
   }
 }
